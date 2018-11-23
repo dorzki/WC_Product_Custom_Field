2f24a7a676801d7b73dbbb1456098e7271e4a0f7
Dor Zuberi <webmaster@dorzki.co.il>
2018-11-23 11:10:33 +0200

    Created base plugin files.

---
+++ class-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * class-plugin.php
+ *
+ * @package    dorzki\WooCommerce
+ * @subpackage Plugin
+ * @author     Dor Zuberi <webmaster@dorzki.co.il>
+ * @link       https://www.dorzki.co.il
+ * @version    1.0.0
+ */
+
+namespace dorzki\WooCommerce;
+
+
+class Plugin {
+
+}
